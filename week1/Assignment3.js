4308d5d5dcdabc131ceda6dfaa48f578f5ab7eff
Bishal1030 <bishal4shahi@gmail.com>
2025-02-19 16:38:47 +0545

    fix: uppercase missing fixed

--- week1/Assignment3.js
+++ week1/Assignment3.js
@@ -2,7 +2,8 @@ function createCode(name, dob){
     const extractedName = name.slice(0,3)
     const extractedDob = dob.slice(2,4)
     const concatString = extractedName.concat(extractedDob)
-    console.log("Output", concatString)
+    const upperCase = concatString.toUpperCase();
+    console.log("Output", upperCase)
 }
 
 createCode("John", "1995-06-15")
