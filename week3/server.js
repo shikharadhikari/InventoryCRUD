316184ad785263b7f6560e7d37c8768ba51477c9
Bibhusan7 <shikharadhikari7@gmail.com>
2025-03-07 18:59:29 +0530

    route errors solved

--- week3/server.js
+++ week3/server.js
@@ -17,7 +17,7 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-app.use("/warehouses", warehouseRoutes);
+app.use("/warehouses", warehouseRoutes.default || warehouseRoutes); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
